8f6bb9a793cf5fa6fe853b7b30573493d871eec9
nexusacademics <jfeldman@nexusacademics.com>
2025-07-22 12:00:44 -0400

    Updated useTestData.ts

--- src/hooks/useTestData.ts
+++ src/hooks/useTestData.ts
@@ -11,7 +11,7 @@ export function useTestData() {
     const processed: { [key: string]: ProcessedPrepTest } = {};
     
     // Process all your raw test data here
-    processed[LSAC140.moduleName] = processRawPrepTest(LSACTest140);
+    processed[LSAC140.moduleName] = processRawPrepTest(LSAC140);
     
     // Add more tests as needed:
     // processed['PrepTest 141'] = processRawPrepTest(rawPrepTest141);
