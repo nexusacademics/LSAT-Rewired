53d1f697bd56e03a2711f475bcb9e55801e0d048
nexusacademics <jfeldman@nexusacademics.com>
2025-07-24 23:39:54 -0400

    Updated index.tsx

--- src/components/TripleReview/index.tsx
+++ src/components/TripleReview/index.tsx
@@ -1,7 +1,7 @@
 // components/TripleReview/index.tsx
 import React, { useState, useEffect } from 'react';
 import { TestSession, ProcessedPrepTest, ProcessedSection, ProcessedQuestion, Circuit } from '../../App';
-import CircuitBuilder from '../ReactFlowCircuitBuilder';
+import ReactFlowCircuitBuilder from '../ReactFlowCircuitBuilder';
 import FloatingCircuitBuilderButton from '../FloatingCircuitBuilderButton'; // ✅ Already present
 import { Header } from './Header';
 import { PassagePanel } from './PassagePanel';
