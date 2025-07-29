a0ebd90d36cedaffc89d08d9c4344dff592c8e55
nexusacademics <jfeldman@nexusacademics.com>
2025-07-29 17:27:36 -0400

    Updated FloatingChatButton.tsx

--- src/components/FloatingChatButton.tsx
+++ src/components/FloatingChatButton.tsx
@@ -69,7 +69,7 @@ const toggleChat = () => {
   drag
   dragMomentum={false}
   dragElastic={0.2}
-  className={`fixed bottom-24 left-20 w-[500px] h-[500px] bg-transparent rounded-2xl shadow-xl border border-slate-200 z-50
+  className={`fixed bottom-24 left-20 w-[500px] h-[500px] bg-transparent rounded-2xl z-50
     transform transition-all duration-300 ease-in-out cursor-move
     ${isOpen ? 'translate-y-0 opacity-100 visible' : 'translate-y-4 opacity-0 invisible'}
   `}
