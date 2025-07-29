1810b862ab6d13976fb93ff271d01ab93721a20d
nexusacademics <jfeldman@nexusacademics.com>
2025-07-29 09:37:35 -0400

    Updated FloatingCircuitBuilderButton.tsx

--- src/components/FloatingCircuitBuilderButton.tsx
+++ src/components/FloatingCircuitBuilderButton.tsx
@@ -38,20 +38,6 @@ export default function FloatingCircuitBuilderButton({
         </div>
       )}
 
-      {/* Success Bubble */}
-      {showSuccessMessage && analysisQualityScore !== undefined && (
-        <div className="bg-green-50 border border-green-200 shadow-lg rounded-xl p-3 w-40 text-sm text-green-800 relative">
-          <div className="flex items-center mb-1">
-            <CheckCircle className="h-4 w-4 mr-2 text-green-700" />
-            <span className="font-semibold">Circuit Created!</span>
-          </div>
-          <p className="text-green-700">
-            Quality Score: {analysisQualityScore}/100
-          </p>
-          <div className="absolute top-1/2 -right-3 w-0 h-0 border-t-8 border-b-8 border-l-8 border-transparent border-l-green-50" />
-        </div>
-      )}
-
       {/* The actual floating button */}
       <button
         onClick={() => {
