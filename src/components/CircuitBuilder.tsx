a88b7120457c67c6d525505f16548e8eccc51952
nexusacademics <jfeldman@nexusacademics.com>
2025-07-30 10:37:21 -0400

    Updated CircuitBuilder.tsx

--- src/components/CircuitBuilder.tsx
+++ src/components/CircuitBuilder.tsx
@@ -353,9 +353,9 @@ const CounterclaimNode = ({ id, data, selected }: NodeProps) => {
   };
 
   return (
-    <div className={px-4 py-3 shadow-md rounded-lg bg-gray-100 border-2 ${
+    <div className={`px-4 py-3 shadow-md rounded-lg bg-gray-100 border-2 ${
       selected ? 'border-red-600' : 'border-red-300'
-    } min-w-[120px] min-h-[70px] relative} 
+    } min-w-[120px] min-h-[70px] relative`} 
        style={
     selected
       ? { boxShadow: '0 0 12px 5px rgba(202, 138, 4, 0.8)' }
