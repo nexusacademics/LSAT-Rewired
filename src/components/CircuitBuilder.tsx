35f706e540bef64e1b41013a28663e4a4ea01416
nexusacademics <jfeldman@nexusacademics.com>
2025-07-30 12:33:55 -0400

    Updated CircuitBuilder.tsx

--- src/components/CircuitBuilder.tsx
+++ src/components/CircuitBuilder.tsx
@@ -617,7 +617,7 @@ const CircuitBuilderFlow = () => {
   }, [setNodes, setEdges]);
 
   return (
-   <div className="flex flex-col bg-slate-50">
+   <div className="flex flex-col bg-slate-50 max-h-400">
       {/* Header */}
       <div className="bg-white border-b border-slate-200 px-6 py-4">
         <div className="flex items-center justify-between">
