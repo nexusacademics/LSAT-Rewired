f26f6be0bb71a4448109acaac24622fd49c4a4bd
nexusacademics <jfeldman@nexusacademics.com>
2025-07-30 10:36:31 -0400

    Updated CircuitBuilder.tsx

--- src/components/CircuitBuilder.tsx
+++ src/components/CircuitBuilder.tsx
@@ -115,9 +115,9 @@ const ConclusionPredicateNode = ({ id, data, selected }: NodeProps) => {
   };
 
   return (
-    <div className={px-4 py-3 shadow-md rounded-lg bg-purple-200 border-2 ${
+    <div className={`px-4 py-3 shadow-md rounded-lg bg-purple-200 border-2 ${
       selected ? 'border-purple-600' : 'border-purple-400'
-    } min-w-[120px] min-h-[70px] relative} 
+    } min-w-[120px] min-h-[70px] relative`} 
        style={
     selected
       ? { boxShadow: '0 0 12px 5px rgba(202, 138, 4, 0.8)' }
