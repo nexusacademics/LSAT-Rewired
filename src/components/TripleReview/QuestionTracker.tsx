e389505092183f71c3a09f3c0010dca0ab2c126d
nexusacademics <jfeldman@nexusacademics.com>
2025-07-30 00:16:17 -0400

    Updated QuestionTracker.tsx

--- src/components/TripleReview/QuestionTracker.tsx
+++ src/components/TripleReview/QuestionTracker.tsx
@@ -213,7 +213,7 @@ export const QuestionTracker: React.FC<QuestionTrackerProps> = ({
   </div>
 
   {/* Question circles */}
-  <div className="flex space-x-3 overflow-x-auto pb-3">
+  <div className="flex space-x-4 overflow-x-auto pb-3">
     {questionsInCurrentSection.map((q, index) => {
       const isCurrent = index === session.currentQuestionIndex;
       const isAnswered = session.answeredQuestions.hasOwnProperty(q.id);
