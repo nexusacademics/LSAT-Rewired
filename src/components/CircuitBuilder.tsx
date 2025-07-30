bcfa06d8229c722421e655011b54016fdb057bfd
nexusacademics <jfeldman@nexusacademics.com>
2025-07-30 09:59:01 -0400

    Updated CircuitBuilder.tsx

--- src/components/CircuitBuilder.tsx
+++ src/components/CircuitBuilder.tsx
@@ -491,7 +491,6 @@ const CircuitBuilderFlow = () => {
     const newEdge: Edge = {
       ...params,
       id: `edge-${params.source}-${params.target}`,
-      markerEnd: 'url(#reactflow__arrowclosed)',
       data: { style: 'solid' },
       type: 'highlightable',
       style: {
