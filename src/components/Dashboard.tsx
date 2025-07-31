26a1e63c095f120dee91830302d4948076f91351
nexusacademics <jfeldman@nexusacademics.com>
2025-07-30 22:05:30 -0400

    Updated Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -163,7 +163,7 @@ const Dashboard: React.FC<DashboardProps> = ({
             {/* Start New Session - Compressed */}
             <Card padding="default" hover>
               <CardHeader className="pb-3">
-                <CardTitle icon={<Target className="h-5 w-5 text-yellow-500" />}>
+                <CardTitle icon={<Zap className="h-5 w-5 text-yellow-500" />}>
                   Start Your Next Session
                 </CardTitle>
               </CardHeader>
@@ -243,7 +243,7 @@ const Dashboard: React.FC<DashboardProps> = ({
           <div className="lg:col-span-1 space-y-4">
             <Card padding="default" hover>
               <CardHeader className="pb-3">
-                <CardTitle icon={<Zap className="h-5 w-5 text-yellow-500" />}>
+                <CardTitle icon={<Flag className="h-5 w-5 text-yellow-500" />}>
                   Start Your Next Drill
                 </CardTitle>
               </CardHeader>
