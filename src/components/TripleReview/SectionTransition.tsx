fa92693177d8d973d860ef1ce83946e3d2d5fb87
nexusacademics <jfeldman@nexusacademics.com>
2025-07-30 21:24:22 -0400

    Updated SectionTransition.tsx

--- src/components/TripleReview/SectionTransition.tsx
+++ src/components/TripleReview/SectionTransition.tsx
@@ -199,7 +199,7 @@ export const SectionTransition: React.FC<SectionTransitionProps> = ({
 
   return (
     <div 
-      className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[100]" 
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[100]" 
       onClick={(e) => {
         if (!showingCountdown) {
           onCancel();
