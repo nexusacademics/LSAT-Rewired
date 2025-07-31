ed45371efb66b730c12717c2a6d99abfa1ca4c5e
nexusacademics <jfeldman@nexusacademics.com>
2025-07-31 08:37:50 -0400

    Updated Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -266,7 +266,7 @@ const Dashboard: React.FC<DashboardProps> = ({
                 <Button 
                   size="default" 
                   className="w-full"
-                  onClick={handleStartNewSessionClick}
+                  onClick={} //Replace with new drill executable
                 >
                   <Play className="h-4 w-4 mr-2" />
                   Start New Drill Session
