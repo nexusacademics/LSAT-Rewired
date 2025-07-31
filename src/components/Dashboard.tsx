d63cc7c18012a5ef98300b60581bc2459010dad3
nexusacademics <jfeldman@nexusacademics.com>
2025-07-30 22:06:07 -0400

    Updated Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -243,7 +243,7 @@ const Dashboard: React.FC<DashboardProps> = ({
           <div className="lg:col-span-1 space-y-4">
             <Card padding="default" hover>
               <CardHeader className="pb-3">
-                <CardTitle icon={<Zap className="h-5 w-5 text-yellow-500" />}>
+                <CardTitle icon={<Brain className="h-5 w-5 text-yellow-500" />}>
                   Start Your Next Drill
                 </CardTitle>
               </CardHeader>
