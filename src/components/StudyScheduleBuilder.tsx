5a96febbd688e0392057dea71c5d366acef113e0
nexusacademics <jfeldman@nexusacademics.com>
2025-08-06 12:46:40 -0400

    Updated StudyScheduleBuilder.tsx

--- src/components/StudyScheduleBuilder.tsx
+++ src/components/StudyScheduleBuilder.tsx
@@ -6,7 +6,9 @@ import GenerateScheduleModal from './GenerateScheduleModal';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import '@fullcalendar/common/main.css';
-import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/core/index.css';
+import '@fullcalendar/daygrid/index.css';
+
 
 
 export default function StudyScheduleBuilder() {
