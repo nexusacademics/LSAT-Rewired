cc9bd60ca7de825f049fc837b08f66f793f3d122
nexusacademics <jfeldman@nexusacademics.com>
2025-08-06 13:12:14 -0400

    Updated StudyScheduleBuilder.tsx

--- src/components/StudyScheduleBuilder.tsx
+++ src/components/StudyScheduleBuilder.tsx
@@ -10,9 +10,9 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
-import '@fullcalendar/common/main.css';
-import '@fullcalendar/daygrid/main.css';
-import '@fullcalendar/timegrid/main.css';
+import '@fullcalendar/common/index.css';
+import '@fullcalendar/daygrid/index.css';
+import '@fullcalendar/timegrid/index.css';
 
 export default function StudyScheduleBuilder() {
   const [currentView, setCurrentView] = useState('dayGridWeek');
