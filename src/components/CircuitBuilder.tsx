e97672368cca0b5ce57d7686e106a0b602dfaf0c
nexusacademics <jfeldman@nexusacademics.com>
2025-08-11 17:55:45 -0400

    Updated CircuitBuilder.tsx

--- src/components/CircuitBuilder.tsx
+++ src/components/CircuitBuilder.tsx
@@ -228,7 +228,7 @@ const MajorPremiseNode = ({ id, data, selected }: NodeProps) => {
        <Handle type="source" position={Position.Left} id="left-source" className="w-3 h-3" />
       
        <Handle type="target" position={Position.Bottom} id="bottom-target" className="w-3 h-3" />
-       <Handle type="source" position={Position.Bottom" id="bottom-source" className="w-3 h-3" />
+       <Handle type="source" position={Position.Bottom} id="bottom-source" className="w-3 h-3" />
       
        <Handle type="target" position={Position.Right} id="right-target" className="w-3 h-3" />
        <Handle type="source" position={Position.Right} id="right-source" className="w-3 h-3" />
