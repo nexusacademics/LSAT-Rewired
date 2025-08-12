247db3cbd7e37080e8e6d7286e3619fd3c75b181
nexusacademics <jfeldman@nexusacademics.com>
2025-08-12 15:07:31 -0400

    Updated PassagePanel.tsx

--- src/components/TripleReview/PassagePanel.tsx
+++ src/components/TripleReview/PassagePanel.tsx
@@ -40,7 +40,13 @@ export const PassagePanel: React.FC<PassagePanelProps> = ({
   lineSpacing
 }) => {
   const passageRef = useRef<HTMLDivElement>(null);
+ const passageText = currentQuestionData.passage || "";
 
+  return (
+    <div className="prose">
+      {highlightText(passageText, searchQuery)}
+    </div>
+  );
   // Get text size classes
   const getTextSizeClass = () => {
     switch (textSize) {
