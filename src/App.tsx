1c82fd683816a624b23a5ffcb43dcb9166fd92d3
nexusacademics <jfeldman@nexusacademics.com>
2025-08-13 01:48:43 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -159,7 +159,7 @@ function AppContent() {
             setLastProcessedQuestionIdForChat={setLastProcessedQuestionIdForChat} // NEW: Pass lifted setter
             hasInitialChatWelcomeBeenSent={hasInitialChatWelcomeBeenSent} // NEW: Pass lifted state
             setHasInitialChatWelcomeBeenSent={setHasInitialChatWelcomeBeenSent} // NEW: Pass lifted setter
-            isStrategySummaryActive={strategySummaryIsActive}
+          
 
           />
         )}
